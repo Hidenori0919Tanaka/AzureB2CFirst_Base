4b60108d5f363921513d65645b170682636f2b4e
HIDENORI TANAKA <hidenori091985@yahoo.co.jp>
2020-08-05 13:23:40 +0900

    add NUnitProject

---
+++ ASPDotNet_B2C/ASPDotNet_B2C.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ASPDotNet_B2C;
+using ASPDotNet_B2C.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace ASPDotNet_B2C.Test.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            //Assert.AreEqual("Your application description page.", result.ViewData.Values);
+        }
+
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
